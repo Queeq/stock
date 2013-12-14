289e0e887794480685127a678209494c80d820ae
Queeq <i.queeq@gmail.com>
2013-12-14 16:45:37 +0200

    First t0pep0 api experiments

--- get_data.py
+++ get_data.py
@@ -1,2 +1,12 @@
 from btceapi import btceapi
-import ConfigParser
+import configparser
+
+config = configparser.ConfigParser()
+config.read('btceapi/config.ini')
+
+api_key = config['DEFAULT']['api_key']
+api_sec = config['DEFAULT']['api_secret']
+
+api = btceapi.api(api_key, api_sec)
+
+print(api.TradeHistory(tfrom=0, tcount=10, tfrom_id=0, tend_id=99999999999, torder='ASC', tsince=0, tend=99999999999999, tpair='btc_usd'))
