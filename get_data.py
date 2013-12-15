931c232d4188eabdc811fc49b16970a70c35cde4
Queeq <i.queeq@gmail.com>
2013-12-15 17:41:37 +0200

    Script to fetch data from Bitcoincharts' API
    get_data.py gets latest data from bitcoincharts.com and appends
    it to given CSV file.
    Currently hardcoded for BTC-e data only.

--- get_data.py
+++ get_data.py
@@ -1,12 +1,49 @@
-from btceapi import btceapi
-import configparser
+#!/usr/bin/python3
 
-config = configparser.ConfigParser()
-config.read('btceapi/config.ini')
+import argparse
+import os
+import sys
+import urllib.request
+import datetime as dt
 
-api_key = config['DEFAULT']['api_key']
-api_sec = config['DEFAULT']['api_secret']
+"""
+This script gets the last data from bitcoincharts.com
+and appends it to existing CSV file from the same site
+"""
 
-api = btceapi.api(api_key, api_sec)
+aparser = argparse.ArgumentParser()
+aparser.add_argument('-f', dest='datafile_path', required=True, help='CSV file to work on')
+args = aparser.parse_args()
 
-print(api.TradeHistory(tfrom=0, tcount=10, tfrom_id=0, tend_id=99999999999, torder='ASC', tsince=0, tend=99999999999999, tpair='btc_usd'))
+# Determine latest timestamp in our datafile
+with open(args.datafile_path, 'rb') as f:
+    offset = 100 # How much bytes to read from the end of file
+    f.seek(0, os.SEEK_END)
+    fsize = f.tell()
+    if fsize == 0:
+        print("Error: file is empty.")
+        sys.exit(1)
+
+    # Read and split into lines
+    f.seek(-1*offset, os.SEEK_END)
+    raw_data = f.read().decode()
+    lines = raw_data.split('\n')
+    # Get last line
+    last_line = lines[-1]
+    last_timestamp = int(last_line.split(',')[0])
+    print("Last available point is at %s" % dt.datetime.fromtimestamp(last_timestamp))
+
+# Get latest data from Bitcoincharts
+btcc_data = urllib.request.urlopen("http://api.bitcoincharts.com/v1/trades.csv?symbol=btceUSD&start=%d" % last_timestamp)
+
+# Form data list of all lines except the first one
+new_data = btcc_data.read().decode().split('\n')[1:]
+newest_timestamp = int(new_data[-1].split(',')[0])
+
+# Append data to file
+print("Appending %s lines to file. Last point is at %s" % (len(new_data), dt.datetime.fromtimestamp(newest_timestamp)))
+
+with open(args.datafile_path, 'a') as f:
+    for line in new_data:
+        f.write("\n"+line)
+
