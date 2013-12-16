a5269d2f2a144343c8e6e027ed47f358273c695c
Queeq <i.queeq@gmail.com>
2013-12-16 16:27:54 +0200

    Bot dev started.
    Basic strategy determined.

---
+++ bot/run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# Get config
+    # short-long, tick size, stop-loss
+
+# Def buy/sell decision
+
+# Def buy/sell simulation
+    # Calculate and log amounts
+
+# Def real buy/sell
+
+
+# Build database for last trades to calculate on
+    # Get data from bicoincharts.com and then those trades which are in between from BTC-e API
+
+# Loop
+
+    # Get latest trades and update DB
+
+    # Calculate averages
+
+    # Run decision function
+
+    # Buy/sell sim or real based on config
+
+    # Log operation
+
