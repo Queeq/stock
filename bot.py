20fa55110e188340c6083b4c89ffb2215d1b25e0
Queeq <i.queeq@gmail.com>
2014-01-06 22:57:35 +0200

    Bot sell/buy timeouts introduced
    Hardcoded to wait for 1/2 of period size before actual buy or sell,
    after corresponding signal is got and not changed.

--- bot.py
+++ bot.py
@@ -22,19 +22,46 @@ res_value = resolutions_convert(res_name)[res_name]
 
 pair = 'btc_usd'
 fee = btceapi.getTradeFee(pair)
+# API returns fee in percent. Get absolute value
+fee /= 100
 
 print("Current fee is", fee)
+print("Trading", pair, "pair")
+print("EMA", fast, slow)
+print("Tick size", res_name)
 
-# Def buy/sell simulation
-    # Calculate and log amounts
+class ActionTimeout(object):
+    """
+    Don't instantly act after signal is generated.
+    Wait 1/2 of period from last signal change to
+    confirm it and actually do what we are supposed to do.
+    """
+    def __init__(self, action, res_value):
+        self.action = action
+        self.res_value = res_value
+        self.trigger_at = float("inf")
 
+    def update(self, signal):
+        # Reset trigger if signal changed
+        if signal != self.action:
+            self.trigger_at = float("inf")
+            print("Reset", self.action, "timeout")
+        # If timer was reset and signal is ours now - set new trigger time
+        elif signal == self.action and self.trigger_at == float("inf"):
+            self.trigger_at = now() + self.res_value/2
+            print("Set", self.action, "timeout to", dt_date(self.trigger_at))
+
+
+# TODO:
 # Def real buy/sell
+    # Get account info
+    # Check if enough USD
+    # Compare and update analytics object (give warning on different values)
 
 
 # Calculate start time for building average
-now = now()
-start_time = now - res_value * slow
-print("Lookback time:", dt.datetime.fromtimestamp(start_time))
+start_time = now() - res_value * slow
+#print("Lookback time:", dt.datetime.fromtimestamp(start_time))
 
 # Fill in initial data from bitcoincharts.com
 working_dataset = Data(res_value)
@@ -47,8 +74,8 @@ for value in new_data:
 # Explicitly update dataset with last values
 working_dataset.update(time, price)
 
-for i, time in enumerate(working_dataset.time):
-    print (dt.datetime.fromtimestamp(time), working_dataset.price[i])
+#for i, time in enumerate(working_dataset.time):
+#    print (dt.datetime.fromtimestamp(time), working_dataset.price[i])
 
 # Analytics object
 act = AveragesAnalytics(res_name, fee, 2)
@@ -56,6 +83,10 @@ act = AveragesAnalytics(res_name, fee, 2)
 # Prepare object data
 act.current_sum = [float(act.startsum), 0.]
 act.buy_allowed = False
+
+# Activate timeout objects
+buy_timeout = ActionTimeout("buy", res_value)
+sell_timeout = ActionTimeout("sell", res_value)
 
 # Loop
 while True:
@@ -82,22 +113,34 @@ while True:
         % (fast_value, slow_value, sar.sar[-1], trend))
 
     #### Simulation ####
-    # If able to buy and buy decision is positive
-    if act.current_sum[0] > 0 \
-      and act.decision('buy', fast_value, slow_value, trend):
-        print("===========%s Buying for %.2f==========="
-            % (dt_date(time), price))
-        act.buy_sell_sim(price, 'buy', act.current_sum)
+    # If buy signal
+    if act.decision('buy', fast_value, slow_value, trend):
+        # Calculate timeouts
+        buy_timeout.update("buy")
+        sell_timeout.update("buy")
 
-    # If able to sell and sell decision is positive
-    if act.current_sum[0] > 0 \
-      and act.decision('sell', fast_value, slow_value, trend):
-        print("===========%s Selling for %.2f==========="
-            % (dt_date(time), price))
-        act.buy_sell_sim(price, 'sell', act.current_sum)
-        print("Current sum is", act.current_sum[0])
+        # If able to buy and buy timeout passed - act
+        if act.current_sum[0] > 0 \
+          and now() > buy_timeout.trigger_at:
+            print("===========%s Buying for %.2f==========="
+                % (dt_date(time), price))
+            act.buy_sell_sim(price, 'buy', act.current_sum)
+        # TODO: Calculate and log amounts
 
-    # Log operation
+    # If sell signal
+    if act.decision('sell', fast_value, slow_value, trend):
+        # Calculate timeouts
+        buy_timeout.update("sell")
+        sell_timeout.update("sell")
+
+        # If able to sell and sell timeout passed - act
+        if act.current_sum[1] > 0 \
+          and now() > sell_timeout.trigger_at:
+            print("===========%s Selling for %.2f==========="
+                % (dt_date(time), price))
+            act.buy_sell_sim(price, 'sell', act.current_sum)
+            # TODO: Calculate and log amounts
+            print("Current sum is", act.current_sum[0])
 
     sleep(10)
 
