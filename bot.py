2a335589c2d75e92e974314767d9eea2ff23ef3f
Queeq <i.queeq@gmail.com>
2014-01-07 12:21:52 +0200

    Bot: slight connection modifications.
    Handle invalid JSON value correctly (this happens when, for example,
    we get 50x errors from web-server).
    Use single connection for all requests to BTC-e.

--- bot.py
+++ bot.py
@@ -20,8 +20,11 @@ stop_loss = float(config['bot']['stop_loss'])
 res_name = config['bot']['resolution']
 res_value = resolutions_convert(res_name)[res_name]
 
+# One connection for everything
+conn = btceapi.common.BTCEConnection()
+
 pair = 'btc_usd'
-fee = btceapi.getTradeFee(pair)
+fee = btceapi.getTradeFee(pair, connection=conn)
 # API returns fee in percent. Get absolute value
 fee /= 100
 
@@ -43,7 +46,7 @@ class ActionTimeout(object):
 
     def update(self, signal):
         # Reset trigger if signal changed
-        if signal != self.action:
+        if signal != self.action and self.trigger_at != float("inf"):
             self.trigger_at = float("inf")
             print("Reset", self.action, "timeout")
         # If timer was reset and signal is ours now - set new trigger time
@@ -91,56 +94,64 @@ sell_timeout = ActionTimeout("sell", res_value)
 # Loop
 while True:
     # Get latest trades and update DB
-    last_trades = btceapi.getTradeHistory(pair, count=100)
-    for t in last_trades:
-        time = dt_timestamp(t.date)
-        working_dataset.update(time, t.price)
+    try:
+        last_trades = btceapi.getTradeHistory(pair, count=100, connection=conn)
+    except ValueError:
+        print("getTradeHistory failed. Skipping actions")
+    else:
+        for t in last_trades:
+            time = dt_timestamp(t.date)
+            working_dataset.update(time, t.price)
 
-    # Calculate averages based on working dataset
-    mas = MovingAverages(working_dataset, (fast, slow), realtime=True)
-    # Calculate SAR for working dataset
-    sar = SAR(working_dataset)
+        # Calculate averages based on working dataset
+        mas = MovingAverages(working_dataset, (fast, slow), realtime=True)
+        # Calculate SAR for working dataset
+        sar = SAR(working_dataset)
 
-    fast_value = mas.ma['exp'][fast][-1]
-    slow_value = mas.ma['exp'][slow][-1]
-    trend = sar.trend[-1]
-    price = working_dataset.price[-1]
-    time = working_dataset.time[-1]
+        fast_value = mas.ma['exp'][fast][-1]
+        slow_value = mas.ma['exp'][slow][-1]
+        trend = sar.trend[-1]
+        price = working_dataset.price[-1]
+        time = working_dataset.time[-1]
 
-    print (dt_date(time), price,
-        working_dataset.high[-1], working_dataset.low[-1],
-        "\tFast: %.2f slow: %.2f SAR: %.2f Trend: %s"
-        % (fast_value, slow_value, sar.sar[-1], trend))
+        '''
+        print (dt_date(time), price,
+            working_dataset.high[-1], working_dataset.low[-1],
+            "\tFast: %.2f slow: %.2f SAR: %.2f Trend: %s"
+            % (fast_value, slow_value, sar.sar[-1], trend))
+        '''
 
-    #### Simulation ####
-    # If buy signal
-    if act.decision('buy', fast_value, slow_value, trend):
-        # Calculate timeouts
-        buy_timeout.update("buy")
-        sell_timeout.update("buy")
+        #### Simulation ####
+        # If buy signal
+        if act.decision('buy', fast_value, slow_value, trend):
+            # Calculate timeouts
+            buy_timeout.update("buy")
+            sell_timeout.update("buy")
 
-        # If able to buy and buy timeout passed - act
-        if act.current_sum[0] > 0 \
-          and now() > buy_timeout.trigger_at:
-            print("===========%s Buying for %.2f==========="
-                % (dt_date(time), price))
-            act.buy_sell_sim(price, 'buy', act.current_sum)
-        # TODO: Calculate and log amounts
-
-    # If sell signal
-    if act.decision('sell', fast_value, slow_value, trend):
-        # Calculate timeouts
-        buy_timeout.update("sell")
-        sell_timeout.update("sell")
-
-        # If able to sell and sell timeout passed - act
-        if act.current_sum[1] > 0 \
-          and now() > sell_timeout.trigger_at:
-            print("===========%s Selling for %.2f==========="
-                % (dt_date(time), price))
-            act.buy_sell_sim(price, 'sell', act.current_sum)
+            # If able to buy and buy timeout passed - act
+            if act.current_sum[0] > 0 \
+              and now() > buy_timeout.trigger_at:
+                print("===========%s Buying for %.2f==========="
+                    % (dt_date(time), price))
+                act.buy_sell_sim(price, 'buy', act.current_sum)
             # TODO: Calculate and log amounts
-            print("Current sum is", act.current_sum[0])
+
+        # If sell signal
+        if act.decision('sell', fast_value, slow_value, trend):
+            # Calculate timeouts
+            buy_timeout.update("sell")
+            sell_timeout.update("sell")
+
+            # If able to sell and sell timeout passed - act
+            if act.current_sum[1] > 0 \
+              and now() > sell_timeout.trigger_at:
+                print("===========%s Selling for %.2f==========="
+                    % (dt_date(time), price))
+                act.buy_sell_sim(price, 'sell', act.current_sum)
+                # TODO: Calculate and log amounts
+                print("Current sum is", act.current_sum[0])
+
+    # End main try-else block
 
     sleep(10)
 
